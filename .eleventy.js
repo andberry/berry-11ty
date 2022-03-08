d3e6f55ebd745b952fca1437ecbe320061f501f3
Berry <andrea@trampolinebranding.com>
2022-03-08 01:04:59 -0400

    Added html-minifier and cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,8 @@
+const htmlMinifier = require('html-minifier');
+
+console.log(htmlMinifier);
+
 module.exports = function (eleventyConfig) {
-    /*
-    eleventyConfig.addCollection("homeContents", collectionApi => {
-        return collectionApi.getFilteredByTag('homeContents').sort((a, b) => {
-            return a.fileSlug.localeCompare(b.fileSlug);
-        });
-    });
-
-    eleventyConfig.addCollection("homeFrContents", collectionApi => {
-        return collectionApi.getFilteredByTag('homeFrContents').sort((a, b) => {
-            return a.fileSlug.localeCompare(b.fileSlug);
-        });
-    });
-    */
-
     eleventyConfig.addFilter("sortCollectionByFilename", (c) => {
         return c.sort((a, b) => {
             const aNum = parseInt(a.fileSlug.split('-')[0]);
@@ -21,10 +11,27 @@ module.exports = function (eleventyConfig) {
             // return a.fileSlug.split('-')[0].localeCompare(b.fileSlug.split('-')[0])
         });
     });
-
+    
+    
+    
     // assets config
     eleventyConfig.addPassthroughCopy({ "src/assets/compiled": "assets" });
     eleventyConfig.addWatchTarget("src/assets/compiled/");
+
+
+
+    // minify html
+    eleventyConfig.addTransform('htmlMinify', (content, filename) => {
+        if (filename && filename.endsWith(".html")) {
+            console.log('minifying')
+            return htmlMinifier.minify(content, {
+                useShortDoctype: true,
+                collapseWhitespace: true,
+                removeComments: true
+            });
+        }
+        return content;
+    })
     
     
     return {
