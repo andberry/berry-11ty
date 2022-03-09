f98e6d6832efbf2c483785058337eb8ec0cc96f0
Berry <andrea@trampolinebranding.com>
2022-03-09 00:00:28 -0400

    js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,16 @@
-console.log('mao pis');
+function initScrollLinks() {
+    const scrollLinks = document.querySelectorAll("a[href^='#']");
+    for (const item of Array.from(scrollLinks)) {
+        UIkit.scroll(item, {});
+    }
+}
+
+function doOnDocumentLoaded () {
+    initScrollLinks();
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', doOnDocumentLoaded);
+} else {
+    doOnDocumentLoaded();
+}
