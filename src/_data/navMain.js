3f3a0b9974f731eef2f927a71022c65e4a2015ba
Berry <andrea@trampolinebranding.com>
2022-03-08 23:58:13 -0400

    Worked on base and general templates

---
+++ src/_data/navMain.js
@@ -0,0 +1,8 @@
+const globals = require('./globals.json');
+
+module.exports = [
+    { "url": "#overview", "text": "Overview" },
+    { "url": "#message-from-the-premier", "text": "Message From The Premier" },
+    { "url": "#promise-1", "text": "Our Promises" },
+    { "url": globals.fullPlanDownloadUrl, "text": "Full Plan", "class": "c-button" }
+]
