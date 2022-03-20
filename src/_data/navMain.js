4aef95d506b5bcbad19c6ea2cdbe9f843149552b
Berry <andrea@trampolinebranding.com>
2022-03-20 02:05:31 -0300

    multilanguage globals and nav

--- src/_data/navMain.js
+++
@@ -1,7 +0,0 @@
-const globals = require('./globals.json');
-
-module.exports = [
-    { "url": "#section1", "text": "Section 1" },
-    { "url": "#section2", "text": "Section 2" },
-    { "url": "#section3", "text": "Section 3" }
-]
